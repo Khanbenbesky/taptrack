99abaee422e853a65efbfad5941fd6065ed60678
Khanbenbesky <besky8489@gmail.com>
2026-02-21 17:55:57 +0530

    Added Attendance Controller

--- attendance-service/src/main/java/com/taptrack/attendanceservice/attendance/controller/AttendanceController.java
+++ attendance-service/src/main/java/com/taptrack/attendanceservice/attendance/controller/AttendanceController.java
@@ -1,4 +1,14 @@
 package com.taptrack.attendanceservice.attendance.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/attendances")
 public class AttendanceController {
+
+    private final Logger logger = LoggerFactory.getLogger(AttendanceController.class);
+
 }
